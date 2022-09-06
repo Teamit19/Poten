fa44575cd5233bc8f570f03193f3d19fc8983cdf
wwyyww <yso00302@gmail.com>
2022-09-07 00:17:55 +0900

    Fix: move heartclub -> club

--- Poten/src/main/java/com/example/poten/service/ClubService.java
+++ Poten/src/main/java/com/example/poten/service/ClubService.java
@@ -1,16 +1,16 @@
 package com.example.poten.service;
 
-import com.example.poten.domain.Board;
 import com.example.poten.domain.Club;
+import com.example.poten.domain.HeartClub;
 import com.example.poten.domain.User;
-import com.example.poten.dto.request.BoardForm;
 import com.example.poten.dto.request.ClubForm;
 import com.example.poten.dto.response.UserResponse;
-import com.example.poten.exception.BoardException;
 import com.example.poten.exception.ClubException;
+import com.example.poten.exception.HeartException;
 import com.example.poten.exception.UserException;
 import com.example.poten.repository.BoardRepository;
 import com.example.poten.repository.ClubRepository;
+import com.example.poten.repository.HeartClubRepository;
 import com.example.poten.repository.UserRepository;
 import javax.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
@@ -24,10 +24,14 @@ public class ClubService {
     private final UserRepository userRepository;
     private final ClubRepository clubRepository;
 
-    public ClubService(BoardRepository boardRepository, UserRepository userRepository,  ClubRepository clubRepository) {
+    private final HeartClubRepository heartClubRepository;
+
+
+    public ClubService(BoardRepository boardRepository, UserRepository userRepository, ClubRepository clubRepository, HeartClubRepository heartClubRepository) {
         this.boardRepository = boardRepository;
         this.userRepository = userRepository;
         this.clubRepository = clubRepository;
+        this.heartClubRepository = heartClubRepository;
     }
 
     public Club findByClubId(Long clubId){
@@ -88,6 +92,35 @@ public class ClubService {
 
         return true;
 
+    }
+
+
+    /**
+     *  동아리 좋아요 누름
+     */
+    public void heartClub(User loginUser, Long clubId) {
+        Club findClubFromRepo = clubRepository.findById(clubId).orElseThrow(() -> new ClubException("존재하지 않는 동아리입니다."));
+
+        // 이미 좋아요 누른 유저인지 확인
+        if(heartClubRepository.findHeartClubByUserAndClub(loginUser, findClubFromRepo).isPresent()) throw new HeartException("이미 좋아요 누른 동아리입니다.");
+
+        HeartClub heartClub = HeartClub.builder()
+                .club(findClubFromRepo)
+                .user(loginUser)
+                .build();
+
+        heartClubRepository.save(heartClub);
+    }
+
+    /**
+     *  피드 좋아요 해제
+     */
+    public void unHeartClub(User loginUser, Long clubId) {
+        Club findClubFromRepo = clubRepository.findById(clubId).orElseThrow(() -> new ClubException("존재하지 않는 동아리입니다."));
+
+        HeartClub heartClub = heartClubRepository.findHeartClubByUserAndClub(loginUser, findClubFromRepo).orElseThrow(() -> new HeartException("좋아요 누르지 않은 동아리입니다."));
+
+        heartClubRepository.delete(heartClub);
     }
 
 
