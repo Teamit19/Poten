13101ce8b049368bd422f74af61918ade9146bf1
HyeM207 <63229014+HyeM207@users.noreply.github.com>
2022-09-06 17:52:33 +0900

    Feat(Service): Board Service

--- Poten/src/main/java/com/example/poten/controller/BoardController.java
+++ Poten/src/main/java/com/example/poten/controller/BoardController.java
@@ -1,32 +1,110 @@
 package com.example.poten.controller;
 
-import com.example.poten.dto.request.SignUpForm;
+import com.example.poten.domain.Board;
+import com.example.poten.domain.Club;
+import com.example.poten.domain.User;
+import com.example.poten.dto.request.BoardForm;
+import com.example.poten.dto.request.BoolResponse;
+import com.example.poten.dto.response.BoardResponse;
+import com.example.poten.dto.response.BoardResponseList;
 import com.example.poten.service.BoardService;
+import com.example.poten.service.ClubService;
+import com.example.poten.service.UserService;
 import io.swagger.annotations.ApiOperation;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/board")
+@RequestMapping("/boards")
 public class BoardController {
     private final BoardService boardService;
+    private final UserService userService;
+    private final ClubService clubService;
 
-    @Autowired
-    public BoardController(BoardService boardService) {
+    public BoardController(BoardService boardService, UserService userService, ClubService clubService) {
         this.boardService = boardService;
+        this.userService = userService;
+        this.clubService = clubService;
     }
 
-    @ApiOperation(value = "게시물 업로드")
+    private void logError(List<FieldError> errors) {
+        log.error("Board Errors = {}", errors);
+    }
+
+    /**
+     * 피드
+     */
+    @ApiOperation(value = "피드 생성")
     @PostMapping("/upload")
-    public void uploadBoard(@RequestBody SignUpForm signUpForm) {
-//        log.info("회원가입 버튼 눌림");
+    public ResponseEntity<?> uploadBoard(HttpServletRequest request, @Valid @RequestBody BoardForm boardForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            final List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            logError(fieldErrors);
+            new ResponseEntity<>(fieldErrors, HttpStatus.BAD_REQUEST);
+        }
+
+        User loginUser = userService.getLoginUser(request); // <<TODO>>  로그인한 사용자 불러오기
+        Club userClub = clubService.findByClubId(boardForm.getClubId()); // <<TODO>> clubId로 club 객체 불러오기
+        Board savedBoard = boardService.saveBoard(loginUser, userClub, boardForm);
+        return ResponseEntity.ok(savedBoard.toResponse());
     }
 
+    @ApiOperation(value = "동아리별 피드 조회")
+    @GetMapping("/club/{clubId}")
+    public ResponseEntity<?> getBoard(HttpServletRequest request, @PathVariable Long clubId) {
+        User loginUser = userService.getLoginUser(request); // <<TODO>>  로그인한 사용자 불러오기
 
+        Club club = clubService.findByClubId(clubId); // <<TODO>> clubId로 club 객체 불러오기
+        List<Board> boardEntityList = boardService.findByClubId(club);
+
+        // DTO로 변환
+        List<BoardResponse> boardResponseList = new ArrayList<>();
+        boardEntityList.forEach(b -> boardResponseList.add(b.toResponse()));
+
+        return ResponseEntity.ok(new BoardResponseList(boardResponseList));
+    }
+
+    @ApiOperation(value = "피드 수정")
+    @PutMapping("/update/{boardId}")
+    public ResponseEntity<?> uploadBoard(HttpServletRequest request,  @PathVariable Long boardId, @Valid @RequestBody BoardForm boardForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            final List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            logError(fieldErrors);
+            new ResponseEntity<>(fieldErrors, HttpStatus.BAD_REQUEST);
+        }
+
+        User loginUser = userService.getLoginUser(request); // <<TODO>>  로그인한 사용자 불러오기
+        Board updatedBoard = boardService.updateBoard(loginUser, boardId, boardForm);
+        return ResponseEntity.ok(updatedBoard.toResponse());
+    }
+
+    @ApiOperation(value = "피드 삭제")
+    @DeleteMapping("/delete/{boardId}")
+    public ResponseEntity deleteBoard(HttpServletRequest request,  @PathVariable Long boardId, @Valid @RequestBody BoardForm boardForm, BindingResult bindingResult) {
+
+        User loginUser = userService.getLoginUser(request); // <<TODO>>  로그인한 사용자 불러오기
+        boolean deleteResult =  boardService.deleteBoard(loginUser, boardId);
+        return ResponseEntity.ok(new BoolResponse(deleteResult));
+    }
+
+    /**
+     * 댓글
+     */
 
 }
