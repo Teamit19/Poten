a74d59cd8b85c0bed102737747fbedc09d3748c8
jeongminhui99 <jeongminhee99@naver.com>
2022-09-13 20:31:09 +0900

    Feat: Image transit

--- Poten/src/main/java/com/example/poten/dto/response/Test.java
+++ Poten/src/main/java/com/example/poten/dto/response/Test.java
@@ -2,9 +2,12 @@ package com.example.poten.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 @AllArgsConstructor
 public class Test {
-    private String testId;
+    //private MultipartFile pic;
+    private String content;
+    private String content2;
 }
